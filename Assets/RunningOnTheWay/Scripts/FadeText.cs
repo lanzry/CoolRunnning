bb30c55c767ea24a3751610aad050af49c8e5468
lanzry <lanxiaoshun@myhexin.com>
2017-05-06 13:24:34 +0800

    init cool running

---
+++ Assets/RunningOnTheWay/Scripts/FadeText.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FadeText : MonoBehaviour
+{
+
+    private float fadeSpeed = 2f;
+    private float allTime;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        allTime += fadeSpeed * Time.deltaTime;
+        GetComponent<Text>().color = new Color(0.5f, 0.5f, 0.5f, Mathf.Sin(allTime)*0.5f);
+    }
+}
